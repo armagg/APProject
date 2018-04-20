0fcd38c06ffc29265edabd20e21eb9c03569c756
omid <omid.mesgarha@gmail.com>
2018-04-20 18:25:58 +0430

    playFields added

--- src/Models/Fields/Field.java
+++ src/Models/Fields/Field.java
@@ -4,8 +4,7 @@ public class Field {
     private Deck deck;
     private GraveYard graveYard;
     private Hand hand;
-    private MonsterField monsterField;
-    private SpellField spellField;
+    private PlayField playField;
     private Gamer gamer;
 
 }
