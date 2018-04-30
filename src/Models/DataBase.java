f9e0280114f75a681e5607701f7ebb36f46acf28
omid <omid.mesgarha@gmail.com>
2018-04-30 10:35:39 +0430

    data base created

--- src/Models/DataBase.java
+++ src/Models/DataBase.java
@@ -7,5 +7,37 @@ public class DataBase {
     Store store;
     int Gil;
     Deck deck;
+    private boolean isUsed = false;
 
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public int getGil() {
+        return Gil;
+    }
+
+    public void setGil(int gil) {
+        Gil = gil;
+    }
+
+    public Deck getDeck() {
+        return deck;
+    }
+
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(boolean used) {
+        isUsed = used;
+    }
 }
