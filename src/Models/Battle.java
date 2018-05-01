4629e32fb41a52e60bca688307c0c386c4954dc1
omid <omid.mesgarha@gmail.com>
2018-05-02 01:04:40 +0430

    models updated

--- src/Models/Battle.java
+++ src/Models/Battle.java
@@ -5,12 +5,7 @@ import Models.Fields.Field;
 import Models.Fields.GraveYard;
 import Models.Fields.Hand;
 public class Battle {
-    private Deck playerDeck;
-    private Deck computerDeck;
-    private GraveYard playerGraveYard;
-    private GraveYard computerGraveYard;
-    private Hand pleyerHand;
-    private Hand computerHand;
+
     private Field playerField;
     private Field computerField;
     private Player player =
@@ -21,53 +16,6 @@ public class Battle {
     }
 
 
-    public Deck getPlayerDeck() {
-        return playerDeck;
-    }
-
-    public void setPlayerDeck(Deck playerDeck) {
-        this.playerDeck = playerDeck;
-    }
-
-    public Deck getComputerDeck() {
-        return computerDeck;
-    }
-
-    public void setComputerDeck(Deck computerDeck) {
-        this.computerDeck = computerDeck;
-    }
-
-    public GraveYard getPlayerGraveYard() {
-        return playerGraveYard;
-    }
-
-    public void setPlayerGraveYard(GraveYard playerGraveYard) {
-        this.playerGraveYard = playerGraveYard;
-    }
-
-    public GraveYard getComputerGraveYard() {
-        return computerGraveYard;
-    }
-
-    public void setComputerGraveYard(GraveYard computerGraveYard) {
-        this.computerGraveYard = computerGraveYard;
-    }
-
-    public Hand getPleyerHand() {
-        return pleyerHand;
-    }
-
-    public void setPleyerHand(Hand pleyerHand) {
-        this.pleyerHand = pleyerHand;
-    }
-
-    public Hand getComputerHand() {
-        return computerHand;
-    }
-
-    public void setComputerHand(Hand computerHand) {
-        this.computerHand = computerHand;
-    }
 
     public Field getPlayerField() {
         return playerField;
