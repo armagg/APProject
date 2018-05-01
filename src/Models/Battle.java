52e63880bc7e8e6c9fc0c6addf4b909510b917f0
omid <omid.mesgarha@gmail.com>
2018-05-02 01:00:51 +0430

    models update

--- src/Models/Battle.java
+++ src/Models/Battle.java
@@ -1,12 +1,91 @@
 package Models;
 
 import Models.Fields.Deck;
+import Models.Fields.Field;
 import Models.Fields.GraveYard;
-
+import Models.Fields.Hand;
 public class Battle {
-    Deck playerDeck;
-    Deck computerDeck;
-    GraveYard playerGraveYard;
-    GraveYard computerGraveYard;
+    private Deck playerDeck;
+    private Deck computerDeck;
+    private GraveYard playerGraveYard;
+    private GraveYard computerGraveYard;
+    private Hand pleyerHand;
+    private Hand computerHand;
+    private Field playerField;
+    private Field computerField;
+    private Player player =
+    private int MP = 0;
 
+    public void nextround() {
+        MP++;
+    }
+
+
+    public Deck getPlayerDeck() {
+        return playerDeck;
+    }
+
+    public void setPlayerDeck(Deck playerDeck) {
+        this.playerDeck = playerDeck;
+    }
+
+    public Deck getComputerDeck() {
+        return computerDeck;
+    }
+
+    public void setComputerDeck(Deck computerDeck) {
+        this.computerDeck = computerDeck;
+    }
+
+    public GraveYard getPlayerGraveYard() {
+        return playerGraveYard;
+    }
+
+    public void setPlayerGraveYard(GraveYard playerGraveYard) {
+        this.playerGraveYard = playerGraveYard;
+    }
+
+    public GraveYard getComputerGraveYard() {
+        return computerGraveYard;
+    }
+
+    public void setComputerGraveYard(GraveYard computerGraveYard) {
+        this.computerGraveYard = computerGraveYard;
+    }
+
+    public Hand getPleyerHand() {
+        return pleyerHand;
+    }
+
+    public void setPleyerHand(Hand pleyerHand) {
+        this.pleyerHand = pleyerHand;
+    }
+
+    public Hand getComputerHand() {
+        return computerHand;
+    }
+
+    public void setComputerHand(Hand computerHand) {
+        this.computerHand = computerHand;
+    }
+
+    public Field getPlayerField() {
+        return playerField;
+    }
+
+    public void setPlayerField(Field playerField) {
+        this.playerField = playerField;
+    }
+
+    public Field getComputerField() {
+        return computerField;
+    }
+
+    public void setComputerField(Field computerField) {
+        this.computerField = computerField;
+    }
+
+    public int getMP() {
+        return MP;
+    }
 }
--- src/Models/Fields/Field.java
+++ src/Models/Fields/Field.java
@@ -7,5 +7,43 @@ public class Field {
     private PlayField playField;
     private Gamer gamer;
 
+    public Deck getDeck() {
+        return deck;
+    }
 
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+
+    public GraveYard getGraveYard() {
+        return graveYard;
+    }
+
+    public void setGraveYard(GraveYard graveYard) {
+        this.graveYard = graveYard;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public PlayField getPlayField() {
+        return playField;
+    }
+
+    public void setPlayField(PlayField playField) {
+        this.playField = playField;
+    }
+
+    public Gamer getGamer() {
+        return gamer;
+    }
+
+    public void setGamer(Gamer gamer) {
+        this.gamer = gamer;
+    }
 }
