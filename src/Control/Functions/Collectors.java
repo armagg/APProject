818d221d27b13b0c820d277ceb08929002333682
omid <omid.mesgarha@gmail.com>
2018-05-02 01:53:34 +0430

    battle is ready

--- src/Control/Functions/Collectors.java
+++ src/Control/Functions/Collectors.java
@@ -1,6 +1,14 @@
 package Control.Functions;
 
+import Models.Cards.Card;
+
+import java.util.ArrayList;
+
+import Models.Battle
 public class Collectors {
 
+    public ArrayList<Card> chooseAllOfOwncard(Battle battle) {
+        ArrayList<Card>=new ArrayList<>(20);
 
+    }
 }
--- src/Models/Cards/Card.java
+++ src/Models/Cards/Card.java
@@ -1,8 +1,16 @@
 package Models.Cards;
 
+import Models.Fields.Place;
 import Models.Thing;
 
 public class Card extends Thing {
+    private Place place;
 
+    public Place getPlace() {
+        return place;
+    }
 
+    public void setPlace(Place place) {
+        this.place = place;
+    }
 }
