460732155853ca94efc79982e04ee0ce4e3c07e1
omid <omid.mesgarha@gmail.com>
2018-05-09 12:51:15 +0430

    mian viewer added

--- src/Models/Fields/SuperField.java
+++ src/Models/Fields/SuperField.java
@@ -3,13 +3,12 @@ package Models.Fields;
 import Models.Cards.Classes.Card;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class SuperField {
 
     ArrayList<Card> cards = new ArrayList<>(5);
 
-    public int getNumberOfCards() {
+    public int GetNumberOfCards() {
         return cards.size();
     }
 
@@ -19,46 +18,16 @@ public class SuperField {
 
     public boolean addCard(Card card) {
         cards.add(card);
-        addToMap(card);
         return true;
-    }
-
-    HashMap<String, Integer> cardsOnField = new HashMap<>();
-
-    void addToMap(Card card){
-        if(cardsOnField.containsKey(card.getName())){
-            int num = cardsOnField.get(card.getName());
-            num++;
-            cardsOnField.put(card.getName(),num);
-        }
-        else{
-            cardsOnField.put(card.getName(),1);
-        }
-    }
-
-    public int getNumberOfSpeicialCard(Card card){
-        return cardsOnField.get(card.getName());
     }
 
     public boolean deleteCard(Card card) {
         if (cards.contains(card)) {
             cards.remove(card);
-            cardsOnField.remove(card.getName());
             return true;
 
         }
         return false;
-    }
-
-    @Override
-    public String toString() {
-        String informations = "";
-        for (int i = 0; i < getNumberOfCards(); i++) {
-            informations = informations.concat(getCards().get(i).toString() + "\n");
-        }
-
-
-        return informations;
     }
 
      /*int cardCounterGetter();
@@ -67,3 +36,4 @@ public class SuperField {
 
 
 }
+
