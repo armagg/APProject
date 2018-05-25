81a42d9b9a6ee95e96b6b9d8e5d718e5556ba351
hasansendani <hasansendani1@gmail.com>
2018-05-25 22:32:39 +0430

    be eshqe omid5

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -1,7 +1,12 @@
 package Control;
 
 import Models.Cards.Classes.Normal;
+import Models.Eqiupments.Amulet;
+import Models.Eqiupments.Item;
+import Models.Eqiupments.amulets.*;
+import Models.Eqiupments.items.*;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -27,6 +32,29 @@ public class Main {
        */ //SpellCasters spellCasters = (SpellCasters) monster.getClass().cast(SpellCasters.class);
 
         //System.out.println(spellCasters.getAP());
+
+    }
+    public static ArrayList<Item> returnItems(){
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new GreaterRestorative());
+        items.add(new LargeHPPtion());
+        items.add(new LargeMPpotion());
+        items.add(new LesserRestorative());
+        items.add(new MediumHPPotion());
+        items.add(new MediumMPPotion());
+        items.add(new SmallHPPotion());
+        items.add(new SmallMPPotion());
+        return items;
+    }
+    public static ArrayList<Amulet> returnAmulets(){
+        ArrayList<Amulet> amulets = new ArrayList<>();
+        amulets.add(new DiamondPendant());
+        amulets.add(new DiamondRing());
+        amulets.add(new GoldPendant());
+        amulets.add(new GoldRing());
+        amulets.add(new IronPendant());
+        amulets.add(new IronRing());
+        return amulets;
 
     }
 
