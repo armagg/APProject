f7cdefe7e586248c63cefff3c7c6dcecf28fc470
omid <omid.mesgarha@gmail.com>
2018-05-25 23:52:34 +0430

    adsjf

--- src/Models/Heroes/demonHero/TowerOfInferno.java
+++ src/Models/Heroes/demonHero/TowerOfInferno.java
@@ -7,4 +7,12 @@ public class TowerOfInferno extends Hero{
     public TowerOfInferno(Inventory inventory){
         super(inventory);
     }
+
+    @Override
+    public void reduceHP(int amount) {
+        amount *= 0.8;
+        super.reduceHP(amount);
+    }
 }
+
+
